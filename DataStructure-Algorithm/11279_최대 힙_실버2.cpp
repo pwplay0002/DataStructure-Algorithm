bb713c5cdb19db7a8bc0611edad02f3332e16783
pwplay0002 <36118521+pwplay0002@users.noreply.github.com>
2022-07-18 15:27:19 +0900

    add heap

---
+++ DataStructure-Algorithm/11279_최대 힙_실버2.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//#include <queue>
+//#define endl '\n'
+//using namespace std;
+//int main()
+//{
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//	priority_queue<int, vector<int>, less<int>> pq;
+//	int count = 0;
+//	cin >> count;
+//	for (int i = 0; i < count; i++)
+//	{
+//		int temp = 0;
+//		cin >> temp;
+//		if (temp == 0)
+//		{
+//			if (pq.empty())
+//			{
+//				cout << 0 << endl;
+//			}
+//			else
+//			{
+//				cout << pq.top() << endl;
+//				pq.pop();
+//			}
+//		}
+//		else
+//		{
+//			pq.push(temp);
+//		}
+//	}
+//	return 0;
+//}
--- DataStructure-Algorithm/main.cpp
+++ DataStructure-Algorithm/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #define endl '\n'
 using namespace std;
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	vector<int> speak;
+	std::make_heap(speak.begin(), speak.end());
+	int count = 0;
+	int first = 0;
+	cin >> count;
+	cin >> first;
+	speak.push_back(first);
+	cout << speak[0] << endl;
+	for (int i = 1; i < count; i++)
+	{
+		int temp = 0;
+		int half = 0;
+		cin >> temp;
+		speak.push_back(temp);
+		std::sort(speak.begin(), speak.end());
+		half = speak.size() / 2;
+		if (speak.size() % 2 == 0)
+		{
+			cout << speak[half-1] << endl;
+		}
+		else
+		{
+			cout << speak[half] << endl;
+		}
+		
+	}
+
 	return 0;
 }
