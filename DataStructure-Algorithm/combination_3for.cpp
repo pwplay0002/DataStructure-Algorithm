51f6e0d550e7ffdb79739bca346257d00b48b7ea
pwplay0002 <36118521+pwplay0002@users.noreply.github.com>
2022-08-23 15:47:19 +0900

    add combination

---
+++ DataStructure-Algorithm/combination_3for.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <vector>
+//#define endl '\n';
+//
+//using namespace std;
+//
+//int n = 5, k = 3, a[5] = { 1, 2, 3, 4, 5 };
+//
+//int main()
+//{
+//	vector<int> b;
+//
+//	// 1
+//	//for (int i = 0; i < n; i++)
+//	//{
+//	//	for (int j = 0; j < i; j++)
+//	//	{
+//	//		for (int k = 0; k < j; k++)
+//	//		{
+//	//			cout << i << " : " << j << " : " << k << endl;
+//	//		}
+//	//	}
+//	//}
+//
+//	// 2
+//	//for (int i = 0; i < n; i++)
+//	//{
+//	//	for (int j = i + 1; j < n; j++)
+//	//	{
+//	//		for (int k = j + 1; k < n; k++)
+//	//		{
+//	//			cout << i << " : " << j << " : " << k << endl;
+//	//		}
+//	//	}
+//	//}
+//	return 0;
+//}
---
+++ DataStructure-Algorithm/combination_recursive.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//#include <vector>
+//#define endl '\n';
+//
+//using namespace std;
+//
+//int n = 5, k = 3, a[5] = { 1, 2, 3, 4, 5 };
+//void print(vector<int> b)
+//{
+//	for (int i : b) cout << i << " ";
+//	cout << endl;
+//}
+//void combination(int start, vector<int> b)
+//{
+//	if (b.size() == k)
+//	{
+//		print(b);
+//		return;
+//	}
+//	for (int i = start + 1; i < n; i++)
+//	{
+//		b.push_back(i);
+//		combination(i, b);
+//		b.pop_back();
+//	}
+//}
+//
+//int main()
+//{
+//	vector<int> b;
+//	combination(-1, b);
+//
+//	return 0;
+//}
