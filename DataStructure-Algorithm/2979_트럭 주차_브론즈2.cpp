3396d52fa7cc2cba5558fbd067d5940feb6ec0cf
pwplay0002 <danhana123@naver.com>
2022-08-27 01:43:41 +0900

    add 2979 solve

---
+++ DataStructure-Algorithm/2979_트럭 주차_브론즈2.cpp
@@ -0,0 +1,57 @@
+#pragma region my solve
+//#include <iostream>
+//#define endl '\n'
+//
+//using namespace std;
+//
+//int arr[101];
+//int abc[4];
+//int main()
+//{
+//	cin >> abc[1] >> abc[2] >> abc[3];
+//	for(int i = 0; i < 3; i++)
+//	{
+//		int temp1, temp2;
+//		cin >> temp1 >> temp2;
+//		arr[temp1]++;
+//		arr[temp2]--;
+//	}
+//
+//	int sum = 0;
+//	int currentTruck = 0;
+//	for(int i : arr)
+//	{
+//		if(i != 0)
+//		{
+//			currentTruck += i;
+//		}
+//		sum += abc[currentTruck] * currentTruck;
+//
+//	}
+//	cout << sum;
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region solve
+//#include <bits/stdc++.h>
+//using namespace std;
+//int A, B, C, a, b, cnt[104], ret;
+//int main() {
+//	cin >> A >> B >> C;
+//	for (int i = 0; i < 3; i++) {
+//		cin >> a >> b;
+//		for (int j = a; j < b; j++)cnt[j]++;
+//	}
+//	for (int j = 1; j < 100; j++) {
+//		if (cnt[j]) {
+//			if (cnt[j] == 1) ret += A;
+//			else if (cnt[j] == 2)ret += B * 2;
+//			else if (cnt[j] == 3)ret += C * 3;
+//		}
+//	}
+//	cout << ret << "\n";
+//	return 0;
+//}
+#pragma endregion
