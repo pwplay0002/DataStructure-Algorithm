b89d338ed6702218c0bb32ccbb23fdb46960f98f
pwplay0002 <36118521+pwplay0002@users.noreply.github.com>
2022-08-26 11:55:25 +0900

    add 10808, 2309 solve

---
+++ DataStructure-Algorithm/10808_알파벳 개수_브론즈4.cpp
@@ -0,0 +1,48 @@
+#pragma region my solve
+
+//#include <iostream>
+//#define endl '\n'
+//using namespace std;
+//
+//int alpha[26];
+//int main()
+//{
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	string temp;
+//	cin >> temp;
+//	for (int i = 0; i < temp.length(); i++)
+//	{
+//		alpha[temp[i] - 97] += 1;
+//	}
+//
+//	for (int i = 0; i < 26; i++)
+//	{
+//		cout << alpha[i] << " ";
+//	}
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region solve
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//typedef long long ll;
+//string str;
+//int cnt[26];
+//int main() {
+//	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//	cin >> str;
+//	for (char a : str) {
+//		cnt[a - 'a']++;
+//	}
+//	for (int i = 0; i < 26; i++) cout << cnt[i] << " ";
+//
+//	return 0;
+//}
+
+#pragma endregion
--- DataStructure-Algorithm/2309_일곱 난쟁이_브론즈1.cpp
+++ DataStructure-Algorithm/2309_일곱 난쟁이_브론즈1.cpp
@@ -1,46 +1,98 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#define endl '\n'
-using namespace std;
+#pragma region my solve (next_permutation)
 
-vector<int> arr;
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#define endl '\n'
+//using namespace std;
+//
+//vector<int> arr;
+//
+//void print(vector<int> v)
+//{
+//	for (int i = 0; i < 7; i++)
+//	{
+//		cout << arr[i] << endl;
+//	}
+//}
+//int main()
+//{
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//	int temp;
+//	int sum = 0;
+//	for (int i = 0; i < 9; i++)
+//	{
+//		cin >> temp;
+//		arr.push_back(temp);
+//	}
+//	sort(arr.begin(), arr.end());
+//	do
+//	{
+//		for (int i = 0; i < 7; i++)
+//		{
+//			sum += arr[i];
+//		}
+//		if (sum == 100)
+//		{
+//			sort(arr.begin(), arr.begin() + 7);
+//			print(arr);
+//			break;
+//		}
+//		else
+//		{
+//			sum = 0;
+//		}
+//	} while (next_permutation(arr.begin(), arr.end()));
+//	return 0;
+//}
 
-void print(vector<int> v)
-{
-	for (int i = 0; i < 7; i++)
-	{
-		cout << arr[i] << endl;
-	}
-}
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int temp;
-	int sum = 0;
-	for (int i = 0; i < 9; i++)
-	{
-		cin >> temp;
-		arr.push_back(temp);
-	}
-	sort(arr.begin(), arr.end());
-	do
-	{
-		for (int i = 0; i < 7; i++)
-		{
-			sum += arr[i];
-		}
-		if (sum == 100)
-		{
-			sort(arr.begin(), arr.begin() + 7);
-			print(arr);
-			break;
-		}
-		else
-		{
-			sum = 0;
-		}
-	} while (next_permutation(arr.begin(), arr.end()));
-	return 0;
-}
+#pragma endregion
+
+#pragma region solve (combination_for)
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#define endl '\n'
+//using namespace std;
+//
+//int sum = 0;
+//int a[9];
+//vector<int> v;
+//pair<int, int> ret;
+//void solve()
+//{
+//	for (int i = 0; i < 9; i++)
+//	{
+//		for (int j = 0; j < i; j++)
+//		{
+//			if (sum - a[i] - a[j] == 100)
+//			{
+//				ret = { i, j };
+//				return;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	ios_base::sync_with_stdio(0);
+//	cin.tie(0);
+//	for (int i = 0; i < 9; i++)
+//	{
+//		cin >> a[i]; sum += a[i];
+//	}
+//	solve();
+//	for (int i = 0; i < 9; i++)
+//	{
+//		if (ret.first == i || ret.second == i) continue;
+//		v.push_back(a[i]);
+//	}
+//	sort(v.begin(), v.end());
+//	for (int i : v) cout << i << endl;
+//
+//	return 0;
+//}
+
+#pragma endregion
