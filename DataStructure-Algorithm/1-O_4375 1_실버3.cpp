c1030360cda3c6b8cc7311d80fa83fc030484390
pwplay0002 <danhana123@naver.com>
2022-09-05 00:49:41 +0900

    add 4375 solve

--- DataStructure-Algorithm/1-O_4375 1_실버3.cpp
+++ DataStructure-Algorithm/1-O_4375 1_실버3.cpp
@@ -1,32 +1,59 @@
-#pragma region my solve
+#pragma region my wrong solve
 
-#include <iostream>
-#include <cmath>
-#include <string>
-#define endl '\n'
-typedef long long ll;
-using namespace std;
-ll temp = 1;
-ll n;
-int cnt = 0;
-int main()
-{
-	cin >> n;
-	while (cin >> n)
-	{
-		for (int j = 1; temp % n != 0; j++)
-		{
-			cnt++;
-			temp = pow(10, j) + temp;
-		}
-		cout << cnt + 1;
-		cnt = 0;
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <cmath>
+//#define endl '\n'
+//typedef long long ll;
+//using namespace std;
+//ll temp = 1;
+//ll n;
+//int cnt = 0;
+//int main()
+//{
+//	cin >> n;
+//	while (cin >> n)
+//	{
+//		for (int j = 1; temp % n != 0; j++)
+//		{
+//			cnt++;
+//			temp = pow(10, j) + temp;
+//		}
+//		cout << cnt + 1;
+//		cnt = 0;
+//	}
+//	return 0;
+//}
 
 #pragma endregion
 
 #pragma region solve
+
+//#include <iostream>
+//using namespace std;
+//#define endl '\n'
+//typedef long long ll;
+//int n;
+//int main()
+//{
+//	while (cin >> n)
+//	{
+//		int cnt = 1, ret = 1;
+//		while (true)
+//		{
+//			if(cnt % n == 0)
+//			{
+//				cout << ret << endl;
+//				break;
+//			}
+//			else
+//			{
+//				cnt = (cnt * 10) + 1;
+//				cnt %= n;
+//				ret++;
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
 #pragma endregion
