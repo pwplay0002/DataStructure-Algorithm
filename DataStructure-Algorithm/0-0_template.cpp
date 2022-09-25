cf7a03951fe10b8a4de5f33e3f7173ba77116151
pwplay0002 <danhana123@naver.com>
2022-09-26 06:54:08 +0900

    add 13913 solve2

---
+++ DataStructure-Algorithm/0-0_template.cpp
@@ -0,0 +1,13 @@
+#pragma region my solve
+
+//#include <iostream>
+//#define endl '\n'
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(0); cin.tie(0);
+//	return 0;
+//}
+
+#pragma endregion
