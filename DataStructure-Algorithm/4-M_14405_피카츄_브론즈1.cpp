65cb6e0c140acde8faf141cc6e41c5159e59f7ac
pwplay0002 <danhana123@naver.com>
2023-02-04 14:23:07 +0900

    add 14405 solve

---
+++ DataStructure-Algorithm/4-M_14405_피카츄_브론즈1.cpp
@@ -0,0 +1,53 @@
+#pragma region my solve
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//string s;
+//int main()
+//{
+//	ios_base::sync_with_stdio(0); cin.tie(0);
+//	cin >> s;
+//	bool check = true;
+//	for(int i = 0; i != s.size() && check;)
+//	{
+//		string temp = s.substr(0, 2);
+//		string temp2 = s.substr(0, 3);
+//		if	(temp == "pi" || temp == "ka") s.erase(0, 2);
+//		else if (temp2 == "chu") s.erase(0, 3);
+//		else check = false;
+//	}
+//	if (!s.empty()) check = false;
+//	if (!check) cout << "NO" << '\n';
+//	else cout << "YES" << '\n';
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region solve
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//typedef long long ll;
+//string s;
+//bool flag = 0;
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	cin >> s;
+//	for (int i = 0; i < s.size(); i++) 
+//	{
+//		if (i < s.size() - 1 && s.substr(i, 2) == "pi" || s.substr(i, 2) == "ka") i += 1;
+//		else if (i < s.size() - 2 && s.substr(i, 3) == "chu") i += 2;
+//		else flag = 1;
+//	}
+//	if (flag) cout << "NO\n";
+//	else cout << "YES\n";
+//	return 0;
+//}
+
+#pragma endregion
